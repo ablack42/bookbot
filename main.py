96625740d735f2f1c8715e4fe34a77443e4eddfa
ablack42 <ablack42@gmail.com>
2024-04-26 10:24:30 -0500

    Added letter count

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    #w_count = get_num_words(text)
+    char_count = get_char_count(text)
+    print (char_count)   
 
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+def get_num_words(doc):
+    words = doc.split()
+    count = len(words)
+    return count
+
+def get_char_count(doc):
+    chars = {}
+    for c in doc:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars 
 
 main()
